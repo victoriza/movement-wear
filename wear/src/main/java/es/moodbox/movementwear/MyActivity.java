0358c45ff71b8a85d2866b65cd0125f3a75d7be8
Victor <victor@fishbrain.com>
2014-10-11 14:03:20 +0200

    Add Accelerometer

--- wear/src/main/java/es/moodbox/movementwear/MyActivity.java
+++ wear/src/main/java/es/moodbox/movementwear/MyActivity.java
@@ -1,24 +1,67 @@
 package es.moodbox.movementwear;
 
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MyActivity extends Activity {
+public class MyActivity extends Activity implements SensorEventListener {
 
-    private TextView mTextView;
+	private TextView mTextView;
+	private SensorManager mSensorManager;
+	private Sensor mAccelerometer;
+	private long lastUpdate = 0;
+	private float last_x, last_y, last_z;
+	private static final int SHAKE_THRESHOLD = 600;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
-        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
-        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
-            @Override
-            public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
-            }
-        });
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_my);
+		final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+		stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+			@Override
+			public void onLayoutInflated(WatchViewStub stub) {
+				mTextView = (TextView) stub.findViewById(R.id.text);
+			}
+		});
+		initSensor();
+
+	}
+	private void initSensor() {
+		mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+		mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+		mSensorManager.registerListener(this, mAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
+		if(mAccelerometer == null) {
+			Toast.makeText(this, "Accelerometer sensor not available", Toast.LENGTH_SHORT).show();
+			finish();
+		}else{
+			//mTextView.setText("We have accelerometer");
+			Toast.makeText(this, "We have accelerometer!!", Toast.LENGTH_SHORT).show();
+		}
+	}
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		Sensor mySensor = event.sensor;
+
+		if (mySensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
+
+
+			float x = event.values[0];
+			float y = event.values[1];
+			float z = event.values[2];
+
+			mTextView.setText("ACC values x:"+x+" y:"+y+" z:"+z);
+		}
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+	}
 }
